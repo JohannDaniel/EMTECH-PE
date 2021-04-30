7ac4dc2dce3af1a2df9d81bbecf74d37865845b4
JohannDaniel <johann.alvarez150484@my.lsgh.edu.ph>
2021-05-01 02:15:43 +0800

    2 files modified

--- pages/contact/contact.ts
+++ pages/contact/contact.ts
@@ -7,30 +7,38 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 height: number;
-  weight: number;
-  calorie: number;
-  calorieMessage: string;
-  gender: string;
-  age: number;
-  activityLevel: number;
+weight: number;
+calorie: number;
+calorieMessage: string;
+gender: string;
+age: number;
+activityLevel: number;
 bmr: number;
- x: string = "testing";
-  y: string = "test also";
+carbohydrates: number;
+proteins: number;
+fats: number;
+carboMessage: string;
+proteinsMessage: string;
+fatsMessage: string;
 
 constructor(public navCtrl: NavController) {}
 
 calculateBMR() {
-  
-    
-    if (this.gender === "m") { 
+
+    if (this.gender = "m") { 
       this.bmr = (10 * this.weight + 6.25 * this.height - 5 * this.age + 5);
     }
-    else if (this.gender === "f") { 
+    else if (this.gender = "f") { 
       this.bmr = (10 * this.weight + 6.25 * this.height - 5 * this.age - 161);
     } 
     else{
       this.bmr = 0.01;
     }
       this.calorie = parseFloat((this.bmr * this.activityLevel).toFixed(2));
+      this.carbohydrates = parseFloat((0.60 * this.calorie) .toFixed(0));
+      this.proteins = parseFloat((.125 * this.calorie) .toFixed(0));
+      this.fats = parseFloat((.275 * this.calorie) .toFixed(0));
     }
+    
     }
+
