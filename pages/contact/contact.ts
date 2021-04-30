3931654641c8416b928410c2a6b1500baabf172c
JohannDaniel <johann.alvarez150484@my.lsgh.edu.ph>
2021-05-01 01:37:43 +0800

    1 file modified

--- pages/contact/contact.ts
+++ pages/contact/contact.ts
@@ -9,12 +9,28 @@ export class ContactPage {
 height: number;
   weight: number;
   calorie: number;
-  carbs: number;
-  protein: number;
-  fats: number;
   calorieMessage: string;
-  constructor(public navCtrl: NavController) {
+  gender: string;
+  age: number;
+  activityLevel: number;
+bmr: number;
+ x: string = "testing";
+  y: string = "test also";
 
-  }
+constructor(public navCtrl: NavController) {}
 
-}
+calculateBMR() {
+  
+    
+    if (this.gender === "m") { 
+      this.bmr = (10 * this.weight + 6.25 * this.height - 5 * this.age + 5);
+    }
+    else if (this.gender === "f") { 
+      this.bmr = (10 * this.weight + 6.25 * this.height - 5 * this.age - 161);
+    } 
+    else{
+      this.bmr = 0.01;
+    }
+      this.calorie = parseFloat((this.bmr * this.activityLevel).toFixed(2));
+    }
+    }
